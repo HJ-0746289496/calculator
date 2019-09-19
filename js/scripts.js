7c11c1c577f793fddca9d894df7fb96573ad26e5
HJ-0746289496 <hamdi469959@gmail.com>
2019-09-19 14:27:55 +0300

    adds changes to the first function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
-var add = function(number1, number2) {
-    return number1 + number2;
-  };
+function add () {
+    
+  
   
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
-  var result = add(number1, number2);
-  alert(result);
+  
+  alert(parseInt(number1 + number2));
+}
+add();
